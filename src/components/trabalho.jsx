e372fb628e43482f5aea76a42ea3352c8b2dcd3d
WeslleySORDev <weslleysordev@gmail.com>
2024-09-15 11:07:26 -0300

    .

--- src/components/trabalho.jsx
+++ src/components/trabalho.jsx
@@ -6,7 +6,7 @@ export function Trabalho({ title, src, alt, left, last }) {
       <div
         className={`flex w-fit items-center gap-6 py-2 relative`}
       >
-        <div className="h-64 w-64 rounded ring-2 ring-yellow-800">
+        <div className="h-64 w-64 rounded">
           <img className="h-full w-full rounded" src={src} alt={alt} />
         </div>
         <span className="font-semibold text-lg uppercase absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-white">
